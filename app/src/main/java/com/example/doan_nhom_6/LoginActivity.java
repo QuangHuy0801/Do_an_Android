53cded340b142ed5c29c3dfd10cff29770eb8f43
huy <voquanghuy08102000@gmail.com>
2024-03-13 13:43:18 +0700

    MainActivity

---
+++ app/src/main/java/com/example/doan_nhom_6/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.doan_nhom_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/example/doan_nhom_6/MainActivity.java
+++ app/src/main/java/com/example/doan_nhom_6/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.doan_nhom_6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(1000);  //Delay of 10 seconds
+                } catch (Exception e) {
+
+                } finally {
+
+                    Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
     }
 }
